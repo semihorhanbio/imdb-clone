41037f529bd022c425734b559e57c26a90f95f45
Semih Orhan <sorhan3.14@gmail.com>
2023-07-06 14:16:09 +0300

    create Header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Header/>
+          {children}
+        </body>
       </html>
     )
   }
