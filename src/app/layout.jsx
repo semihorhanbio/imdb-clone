440b3a08661212b6e62bdb135979bedf04d70820
Semih Orhan <sorhan3.14@gmail.com>
2023-07-06 15:27:09 +0300

    create dark mode functionality

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,14 @@
 import Header from "@/components/Header";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
         <body>
-          <Header/>
-          {children}
+          <Providers>
+            <Header/>
+            {children}
+          </Providers> 
         </body>
       </html>
     )
