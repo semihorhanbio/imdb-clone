05a5f50780b4d553e96c122c8b7753e0813b18ca
Semih Orhan <sorhan3.14@gmail.com>
2023-07-09 12:44:59 +0300

    fix input field

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -9,6 +9,7 @@ export default function SearchBox() {
     e.preventDefault() 
     if (!search) return 
     router.push(`/search/${search}`) 
+    setSearch('')
   }
   return (
     <form
@@ -19,7 +20,7 @@ export default function SearchBox() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         type="text"
-        placeholder="Search keywords..."
+        placeholder="Search movies..."
         className="w-full h-14 rounded-sm placeholder-gary-500 outline-none bg-transparent flex-1"
       />
       <button
